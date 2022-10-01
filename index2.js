15afd02b798a70cf34e36219114fab4cb64f3ca4
shiv361 <shivampandey361@gmail.com>
2022-10-01 14:23:07 +0530

    first change background project

---
+++ index2.js
@@ -0,0 +1,6 @@
+const button2 = document.querySelector(".btn")
+const body = document.querySelector('body')
+button2.addEventListener("click", () => {
+    body.style.backgroundColor = 'red'
+    console.log("yes u r correct")
+})
